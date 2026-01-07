4df8c9d1230b3815559e9aebb993b1e225fe2efd
cnouguier <christophe.nouguier@kalisio.com>
2026-01-08 00:17:27 +0100

    test: added tests

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,8 +1,27 @@
-import { util, expect, assert } from 'chai'
-import chailint from 'chai-lint'
+import * as chai from 'chai'
+import { graphiks } from '../src/graphiks.js'
+
+const { expect } = chai
 
 describe('shapes', () => {
+  let Graphiks
+
   before(() => {
-    chailint(chai, util)
+    Graphiks = graphiks()
+  })
+
+  it('should list the shapes', () => {
+    const shapes = Graphiks.listShapes()
+    expect(shapes).to.be.a('array')
+  })
+
+  it('should have the circle shape registered', () => {
+    const hasCircle = Graphiks.hasShape('circle')
+    expect(hasCircle).to.equal(true)
+  })
+
+  it('should not have the dummy shape registered', () => {
+    const hasDummy = Graphiks.hasShape('dummy')
+    expect(hasDummy).to.equal(false)
   })
 })
